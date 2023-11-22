eb60806b569458988ded0098e625222a842e1d13
isjcliu <isjcliu@outlook.com>
2023-11-22 09:39:25 +0000

    23/11/22

--- src/main/scala/operator/Powmod.scala
+++ src/main/scala/operator/Powmod.scala
@@ -17,18 +17,20 @@ class Powmod(val exp_width: Int, val mod_width: Int) extends Module{
     val din  = IO(Flipped(Decoupled(new PowmodInput(exp_width, mod_width))))
     val dout = IO(Decoupled(new PowmodOutput(mod_width)))
     
+    val y     = RegInit(0.U(mod_width.W))
     val x     = RegInit(0.U(mod_width.W))
+    val m     = RegInit(0.U(mod_width.W))
     val e     = RegInit(0.U(mod_width.W))
     val ty    = RegInit(0.U(mod_width.W))
-    val mod   = RegInit(0.U(mod_width.W))
     val exp2k = RegInit(0.U(mod_width.W))
     val res   = RegInit(0.U(mod_width.W))
     
     val i = RegInit(0.U((log2Ceil(mod_width) + 1).W))
     val k = RegInit(mod_width.U((log2Ceil(mod_width) + 1).W))
     
-    val idle :: init :: conv :: s1 :: g1 :: s2 :: g2 :: s3 :: g3 :: end :: Nil = Enum(9)
-    
+    val idle :: init :: s1 :: s2 :: s3 :: s4 :: s5 :: s6 :: end :: Nil = Enum(9)
+    val status = RegInit(idle)
+
     din.ready     := false.B
     dout.valid    := false.B
     dout.bits.res := DontCare
@@ -36,127 +38,73 @@ class Powmod(val exp_width: Int, val mod_width: Int) extends Module{
     val mp = Module(new ModMul(mod_width))
     mp.din.bits.mult     := 0.U
     mp.din.bits.multcand := 0.U
-    mp.din.bits.mod      := din.bits.mod
+    mp.din.bits.mod      := 0.U
     mp.din.valid         := false.B
     mp.dout.ready        := false.B
-    
+
     switch(status){
-        is(idle){
-            when(din.ready && din.valid){
-                status           := init
-                mp.din.ready := true.B
-            }.otherwise{
-                status := idle
-            }
-        }
         is(init){
             x                    := din.bits.exp
             e                    := din.bits.expk
             mp.din.bits.mult     := din.bits.base
             mp.din.bits.multcand := din.bits.exp2k
             mp.din.bits.mod      := din.bits.mod
-
-            when(mp.dout.valid === true.B){
+            mp.din.valid         := true.B
+            status               := s1
+        }
+        is(s1){
+            when(mp.dout.ready === true.B){
                 ty     := mp.dout.bits.res
-                status := s1
+                status := s2
             }.otherwise{
                 status := init
             }
+
         }
-        is(s1){
-            when(x[i] === 1.U){
+        is(s2){
+            when(x(i) === 1.U){
                 mp.din.bits.mult     := e
                 mp.din.bits.multcand := ty
-                mp.din.bits.mod      := mod
-                when(mp.dout.valid === true.B){
-                    e := mp
+                mp.din.valid         := true.B
+                status               := s3
+            }.otherwise{
+                status := s4
+            }
+        }
+        is(s3){
+            when(mp.dout.ready === true.B){
+                e      := mp.dout.bits.res
+                status := s4
+            }.otherwise{
+                status := s2
+            }
+        }
+        is(s4){
+            mp.din.bits.mult     := ty
+            mp.din.bits.multcand := ty
+            mp.din.valid         := true.B
+        }
+        is(s5){
+            when(mp.dout.ready === true.B){
+                ty := mp.dout.bits.res
+                when(i =/= k){
+                    i >> 1.U
+                    status := s2
+                }.otherwise{
+                    status := s6
                 }
             }
         }
-        is(s2)
+        is(s6){
+            mp.din.bits.mult     := 1.U
+            mp.din.bits.multcand := e
+            mp.din.valid         := true.B
+            status               := end 
+        }
+        is(end){
+            when(mp.dout.valid === true.B){
+                res := mp.dout.bits.res
+            }
+        }
     }
-    // switch(status){
-    //     is(IDLE){//0
-    //         when(din.ready && din.valid){
-    //             status := INIT
-    //         }.otherwise{
-    //             status := IDLE
-    //         }
-    //     }
-    //     is(INIT){//1
-    //         exp2k := din.bits.exp2k
-    //         y := din.bits.base
-    //         x := din.bits.exp
-    //         m := din.bits.mod
-    //         e := din.bits.expk
-    //         count := 0.U
-    //         status := CONV
-    //     }
-    //     is(CONV){//2
-    //         modMul.din.valid := true.B
-    //         status := SA1
-    //     }
-    //     is(SA1){//3
-    //         modMul.din.bits.mult := y
-    //         modMul.din.bits.multcand := exp2k
-    //         modMul.din.bits.mod := m
-    //         when(modMul.dout.valid === true.B){
-    //             status := GA1
-    //         }
-    //     }
-    //     is(GA1){//5
-  
-    //         y := modMul.dout.bits.res
-    //         status := S1
-    //     }
-    //     is(S1){//6
-    //         when(x(count) === 1.U){
-    //             modMul.din.valid := true.B
-    //             status := SA2
-    //         }.otherwise{
-    //             status := S2
-    //         }
-    //     }
-    //     is(SA2){//7
-    //         modMul.din.bits.mult := e
-    //         modMul.din.bits.multcand := y
-    //         modMul.din.bits.mod := m
-    //         modMul.dout.ready := true.B    
-    //         status := GA2
-    //     }
-    //     is(GA2){//8
-    //         e := modMul.dout.bits.res
-    //         status := S2
-    //     }
-    //     is(S2){//9
-    //         modMul.din.bits.mult := y
-    //         modMul.din.bits.multcand := y
-    //         modMul.din.bits.mod := m
-    //         modMul.din.valid := true.B
-    //         modMul.dout.ready := true.B
-    //         status := S3
-    //     }
-    //     is(S3){//10
-    //         y := modMul.dout.bits.res
-    //         when(count === countMax){
-    //             modMul.din.bits.mult := 1.U
-    //             modMul.din.bits.multcand := e
-    //             modMul.din.bits.mod := m
-    //             modMul.din.valid := true.B
-    //             modMul.dout.ready := true.B
-    //             status := END
-    //         }.otherwise{
-    //             count := count + 1.U
-    //             status := S1
-    //         }
-    //     }
-    //     is(END){//11
-    //         res := modMul.dout.bits.res 
-    //         dout.valid := true.B
-    //         status := IDLE
-    //     }
-    // }
-    // when(dout.ready && dout.valid){
-    //     dout.bits.res := res
-    // }
 }
--- src/test/scala/PowmodTB.scala
+++ src/test/scala/PowmodTB.scala
@@ -1,22 +1,22 @@
-// package test
+package test
 
-// import operator._
-// import chisel3._
-// import chiseltest._
-// import org.scalatest.freespec.AnyFreeSpec
+import operator._
+import chisel3._
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
 
-// class PowmodTB extends AnyFreeSpec with ChiselScalatestTester{
-//     "DUT should calculate" in{
-//         test(new Powmod(8, 8)).withAnnotations(Seq(WriteVcdAnnotation)){dut =>
-//         dut.clock.setTimeout(0)
-//         dut.din.bits.base.poke(236.U)
-//         dut.din.bits.exp.poke(202.U)
-//         dut.din.bits.expk.poke(17.U)
-//         dut.din.bits.exp2k.poke(50.U)
-//         dut.din.bits.mod.poke(239.U)
-//         dut.din.valid.poke(true.B)
-//         dut.dout.ready.poke(true.B)
-//         dut.clock.step(1024)
-//         }
-//     }
-// }
+class PowmodTB extends AnyFreeSpec with ChiselScalatestTester{
+    "DUT should calculate" in{
+        test(new Powmod(8, 8)).withAnnotations(Seq(WriteVcdAnnotation)){dut =>
+        dut.clock.setTimeout(0)
+        dut.din.bits.base.poke(236.U)
+        dut.din.bits.exp.poke(202.U)
+        dut.din.bits.expk.poke(17.U)
+        dut.din.bits.exp2k.poke(50.U)
+        dut.din.bits.mod.poke(239.U)
+        dut.din.valid.poke(true.B)
+        dut.dout.ready.poke(true.B)
+        dut.clock.step(1024)
+        }
+    }
+}
